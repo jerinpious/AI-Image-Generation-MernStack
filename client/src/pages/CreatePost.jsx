43ea37857de483eb7d3a10a0e7ae5b3e4eeb2583
jerinpious <jpious4925@conestogac.on.ca>
2023-12-24 19:10:54 -0500

    FormFields

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -7,10 +7,75 @@ import { FormFields,Loader } from '../components';
 
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form,setform] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+  });
+
+  const [generatingImg,setgeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {
+
+  }
+  const handleChange =(e) =>{
+
+  }
+  const handleSurpriseMe=() => {
+
+  }
+
   return (
+  <section className="max-w-7xl mx-auto">
     <div>
-     
-    </div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">Create imaginative and
+          visually stunning images generated through DALL-E AI and share them with the community</p>
+     </div>
+
+     <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormFields
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormFields 
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+          focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img src={form.photo} alt={form.prompt}
+              className="w-full h-full object-contain" />
+            ): (
+              <img src={preview} alt="preview"
+              className="w-9/12 h-9/12 object-contain opacity-40" />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)]
+              rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+     </form>
+
+  </section>
   )
 }
 
