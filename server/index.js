c499e36fab5e125229faea406558f04b65db9b25
jerinpious <jpious4925@conestogac.on.ca>
2023-12-25 17:53:30 -0500

    Database

--- server/index.js
+++ server/index.js
@@ -2,4 +2,27 @@ import express from'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }))
+
+app.get('/',async (req,res) => {
+    res.send('Hello from DALL-E!');
+})
+
+const startServer = async() => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(5000, () => console.log('Server has started on port http://localhost:5000'))
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+startServer();
